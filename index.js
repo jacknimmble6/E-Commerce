c1d51710e99cee1a8ee3541144e3730a7d224d4e
jack nimmble <jacknimmble@gmail.com>
2022-03-07 03:16:21 -0500

    make it better

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use('/p', routes, haltOnTimedout)
+app.use('/p', routes)
 
 app.get('/', (req, res) => {
   res.send('Hello to E-Commerce API!')
