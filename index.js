b60325b03c3dc5af9354dfed00d70d47850054ca
jack nimmble <jacknimmble@gmail.com>
2022-03-07 03:40:51 -0500

    make it better

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
   res.send('Hello to E-Commerce API!')
 })
 
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT|| 5000
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
